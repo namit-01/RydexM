33fd6e8e535c28c1de68ca4f221a9fee2c8ae59d
Namit <chaturvedinamit899@gmail.com>
2026-07-09 16:50:41 +0530

    bsxwhdg

--- src/app/user/checkout/page.tsx
+++ src/app/user/checkout/page.tsx
@@ -1,11 +1,14 @@
 "use client";
-import CheckOutContent from "@/components/CheckOutContent";
-import React, { Suspense } from "react";
 
-const page = () => {
-  <Suspense fallback={<div>Loading...</div>}>
-    <CheckOutContent />
-  </Suspense>;
+import React, { Suspense } from "react";
+import CheckOutContent from "@/components/CheckOutContent";
+
+const Page = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CheckOutContent />
+    </Suspense>
+  );
 };
 
-export default page;
+export default Page;
